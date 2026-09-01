23761ba67db0d81856b66d44bbaa3e67ccbe2f80
Shawana73 <shawanagohar39@gmail.com>
2026-09-02 01:04:45 +0500

    Restructure project into separate admin and applicant apps

--- android/app/src/main/kotlin/com/shawana/app/my_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.shawana.app.my_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- digital_housing_society_flutter_app/android/app/src/main/kotlin/com/example/digital_housing_society/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.digital_housing_society
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
